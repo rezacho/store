6122bef48c36ca0c786db36346b3173a9cf3a4e3
rezacho <reza.chookian@gmail.com>
2022-04-09 14:02:49 +0430

    add (accounts, home) to INSTALLED_APPS

--- store/settings.py
+++ store/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
